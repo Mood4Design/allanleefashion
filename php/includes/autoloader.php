1afa04b9e130c3cdff1014db9f16cdabba7cc637
allan69 <leallan69@gmail.com>
2021-06-01 20:14:24 +1000

    Revert "AllanLee Website"
    This reverts commit 1fff2b0f2971c11b2496918fed7fa4fe01ff016b.

--- php/includes/autoloader.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function __autoload($className)
-{
-    $className = str_replace('\\','/',$className);
-    $className = $className . ".php";
-    require_once(ROOT . PROJECT . "php/" . $className);
-}
--- php/includes/error.php
+++
@@ -1,3 +0,0 @@
- <header>		
-<h3 class="container text-center page-header"><?= $this->error ?></h3> 
-</header> 
